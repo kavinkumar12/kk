a78583dc680ecba21624fc586408a08c021ab16e
kavinkumar12 <42169917+kavinkumar12@users.noreply.github.com>
2019-07-01 15:55:20 +0530

    Create kavin.py

---
+++ kavin.py
@@ -0,0 +1,8 @@
+def swap_numbers(a,b):
+    temp=a
+    a=b
+    b=temp
+    print("After Swapping two Number: num1={0} and num2={1}".format(a, b))
+    num1 = float(input())
+    num2 = float(input())
+    print("before swaping two numbers num1={0} and num={1}")
